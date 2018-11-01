86db98ff9a1c6e9e42e54b3326ca3758a0e3ac7b
dangowans <d.gowans@cityssm.on.ca>
2018-11-01 13:47:04 -0400

    test search
    to help with debugging

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
-(function() {
+/* global document */
 
+(function() {
+  "use strict";
+
+  const searchStrEle = document.getElementById("searchStr");
+  const searchResultsEle = document.getElementById("searchResults");
+
+  function displaySearch() {
+
+    const searchValue = searchStrEle.value.trim();
+
+    if (searchValue === "") {
+      searchResultsEle.innerHTML = "";
+    }
+
+    searchResultsEle.innerHTML = "<div class=\"list-group-item\">" +
+      "Searching... <i class=\"fas fa-pulse fa-spinner\" aria-hidden=\"true\"></i>" +
+      "</div>";
+
+    $.get("data/itemsJSON.asp", {
+        q: searchStrEle.value
+      },
+      function(json) {
+
+        if (json.items.length === 0) {
+          searchResultsEle.innerHTML = "<div class=\"list-group-item list-group-item-info text-center\">" +
+            "Your search returned no results." +
+            "</div>";
+
+          return;
+        }
+
+        searchResultsEle.innerHTML = json.items.reduce(function(soFar, itemJSON) {
+          return soFar + ("<a class=\"list-group-item\" href=\"#" + itemJSON.itemKey + "\">" +
+            itemJSON.itemName + "<br />" +
+            "<small>" + itemJSON.shortDescription + "</small>" +
+            "</a>");
+        }, "");
+
+      }, "json");
+  }
+
+  searchStrEle.addEventListener("keyup", displaySearch);
 }());
